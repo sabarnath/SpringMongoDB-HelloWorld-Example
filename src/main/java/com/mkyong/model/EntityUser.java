287b85a3c2893320cb65b20a16c6ed7241d81415
sabarinathan soundararajan <sabarnath@gmail.com>
2016-06-20 19:53:30 +0530

    entity domin added.

---
+++ src/main/java/com/mkyong/model/EntityUser.java
@@ -0,0 +1,43 @@
+package com.mkyong.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "entity_users")
+public class EntityUser {
+
+	@Id
+	private String id;
+
+	private EmployeeVO employee;
+	
+	private User user;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public EmployeeVO getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(EmployeeVO employee) {
+        this.employee = employee;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+	
+
+	
+
+}
